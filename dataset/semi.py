bee5ed7b9a8efb8b77c641895fdd776737fdac59
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2024-08-14 09:25:10 +0900

    Update semi.py

--- dataset/semi.py
+++ dataset/semi.py
@@ -11,33 +11,44 @@ import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-
 class SemiDataset(Dataset):
-    def __init__(self, name, root, mode, size=None, id_path=None, nsample=None):
+    def __init__(self, name, root, mode, size=None):
         self.name = name
-        self.root = root
+        # self.root = root
         self.mode = mode
         self.size = size
 
-        if mode == 'train_l' or mode == 'train_u':
-            with open(id_path, 'r') as f: # 아마도 id담긴 txt file
-                self.ids = f.read().splitlines()
-            if mode == 'train_l' and nsample is not None:
-                self.ids *= math.ceil(nsample / len(self.ids))
-                random.shuffle(self.ids)
-                self.ids = self.ids[:nsample]
-        elif mode == "val":
-            with open('partitions/%s/val.txt' % name, 'r') as f:
-                self.ids = f.read().splitlines()
+        if mode == "train_l":
+            self.root = os.path.join(root, "train")
+            self.ids = os.listdir(os.path.join(self.root,"target"))
+        elif mode == "train_u":
+            self.root = os.path.join(root, "train")
+            self.ids = list(set(os.listdir(os.path.join(self.root, "input"))) - set(os.listdir(os.path.join(self.root, "target"))))
+        elif mode == "test":
+            self.root = os.path.join(root, "test")
+            self.ids = os.listdir(os.path.join(self.root, "target"))
         else:
-            raise ValueError("%s is not available mode"%(mode))
+            raise ValueError("%s is not available mode", mode)
+        # if mode == 'train_l' or mode == 'train_u':
+        #     with open(id_path, 'r') as f:
+        #         self.ids = f.read().splitlines()
+        #     if mode == 'train_l' and nsample is not None:
+        #         self.ids *= math.ceil(nsample / len(self.ids))
+        #         random.shuffle(self.ids)
+        #         self.ids = self.ids[:nsample]
+        # else:
+        #     with open('partitions/%s/val.txt' % name, 'r') as f:
+        #         self.ids = f.read().splitlines()
 
     def __getitem__(self, item):
         id = self.ids[item]
-        img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
-        mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
+        img = Image.open(os.path.join(self.root, "input", id)).convert('RGB')
+        if (self.mode == "train_l") or (self.mode == "test"):
+            mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, "target", id))))
+        else:
+            mask = Image.fromarray(np.zeros(img.size))
 
-        if self.mode == 'val':
+        if self.mode == 'val' or self.mode == "test":
             img_ori = np.array(img)
             img, mask = normalize(img, mask)
             return img, mask, id, img_ori
@@ -46,10 +57,12 @@ class SemiDataset(Dataset):
         ignore_value = 254 if self.mode == 'train_u' else 255
         img, mask = crop(img, mask, self.size, ignore_value)
         img, mask = hflip(img, mask, p=0.5)
-
-        if self.mode == 'train_l':
+        
+        if self.mode == "train_l" :
             return normalize(img, mask)
-
+        if self.mode == "test":
+            return normalize(img, mask), id
+                
         img_w, img_s1, img_ori = deepcopy(img), deepcopy(img), deepcopy(img)
         img_ori = np.array(img_ori)
 
@@ -71,3 +84,62 @@ class SemiDataset(Dataset):
 
     def __len__(self):
         return len(self.ids)
+
+
+# class SemiDataset(Dataset):
+#     def __init__(self, name, root, mode, size=None, id_path=None, nsample=None):
+#         self.name = name
+#         self.root = root
+#         self.mode = mode
+#         self.size = size
+
+#         if mode == 'train_l' or mode == 'train_u':
+#             with open(id_path, 'r') as f:
+#                 self.ids = f.read().splitlines()
+#             if mode == 'train_l' and nsample is not None:
+#                 self.ids *= math.ceil(nsample / len(self.ids))
+#                 random.shuffle(self.ids)
+#                 self.ids = self.ids[:nsample]
+#         else:
+#             with open('partitions/%s/val.txt' % name, 'r') as f:
+#                 self.ids = f.read().splitlines()
+
+#     def __getitem__(self, item):
+#         id = self.ids[item]
+#         img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
+#         mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
+
+#         if self.mode == 'val':
+#             img_ori = np.array(img)
+#             img, mask = normalize(img, mask)
+#             return img, mask, id, img_ori
+
+#         img, mask = resize(img, mask, (0.5, 2.0))
+#         ignore_value = 254 if self.mode == 'train_u' else 255
+#         img, mask = crop(img, mask, self.size, ignore_value)
+#         img, mask = hflip(img, mask, p=0.5)
+
+#         if self.mode == 'train_l':
+#             return normalize(img, mask)
+
+#         img_w, img_s1, img_ori = deepcopy(img), deepcopy(img), deepcopy(img)
+#         img_ori = np.array(img_ori)
+
+#         if random.random() < 0.8:
+#             img_s1 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s1)
+#         img_s1 = transforms.RandomGrayscale(p=0.2)(img_s1)
+#         img_s1 = blur(img_s1, p=0.5)
+#         cutmix_box1 = obtain_cutmix_box(img_s1.size[0], p=0.5)
+
+
+#         ignore_mask = Image.fromarray(np.zeros((mask.size[1], mask.size[0])))
+
+#         img_s1, ignore_mask = normalize(img_s1, ignore_mask)
+
+#         mask = torch.from_numpy(np.array(mask)).long()
+#         ignore_mask[mask == 254] = 255
+
+#         return normalize(img_w), img_s1, img_ori, ignore_mask, cutmix_box1, id
+
+#     def __len__(self):
+#         return len(self.ids)
